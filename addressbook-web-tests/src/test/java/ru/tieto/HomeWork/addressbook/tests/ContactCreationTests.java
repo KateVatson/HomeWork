a64d95791e2ee29740a07dab0db6f27d55a56e3f
KateVatson <katevatson@icloud.com>
2021-09-12 22:30:06 +0300

    Параметризованы тесты для создания групп и контактов
    ДЗ 13

--- addressbook-web-tests/src/test/java/ru/tieto/HomeWork/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/tieto/HomeWork/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,7 @@
 package ru.tieto.HomeWork.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -34,7 +36,22 @@ public class ContactCreationTests extends TestBase{
     List<ContactData> groups = (List<ContactData>) xstream.fromXML(xml);
     return groups.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
   }
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("src\\test\\java\\ru\\tieto\\HomeWork\\addressbook\\resources\\contacts.json"));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+    }.getType()); // List<ContactData>.class
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
 
+  // Реализован xml
   @Test (dataProvider = "validContactsFromXml")
   public void testContactCreationTests(ContactData contact) {
     Contacts before = app.contact().all();
