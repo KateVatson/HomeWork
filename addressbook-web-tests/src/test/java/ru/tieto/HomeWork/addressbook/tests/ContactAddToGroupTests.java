019248bd61289dff43a3009098161815f29a9945
KateVatson <katevatson@icloud.com>
2021-11-22 20:55:38 +0300

    Исправление ДЗ № 16

--- addressbook-web-tests/src/test/java/ru/tieto/HomeWork/addressbook/tests/ContactAddToGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/tieto/HomeWork/addressbook/tests/ContactAddToGroupTests.java
@@ -7,6 +7,9 @@ import ru.tieto.HomeWork.addressbook.model.Contacts;
 import ru.tieto.HomeWork.addressbook.model.GroupData;
 import ru.tieto.HomeWork.addressbook.model.Groups;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -31,15 +34,15 @@ public class ContactAddToGroupTests extends TestBase {
   @Test
   public void testAddContactToGroup() throws Exception{
     app.goTo().goToHome();
-    ContactData contact = getContact();
+    ContactData contact = getContactData();
     Groups before = contact.getGroups();
-    GroupData groupForAdd = app.db().groups().iterator().next();
+    GroupData groupForAdd = getGroupData(contact);
     app.contact().addContactToGroup(contact, groupForAdd);
     Groups after = app.db().getContactsFromDbById(contact.getId()).getGroups();
     assertThat(after, equalTo(before.withAdded(groupForAdd)));
   }
 
-  public ContactData getContact() {
+  public ContactData getContactData() {
     Contacts contacts = app.db().contacts();
     Groups groups = app.db().groups();
     for (ContactData contact : contacts) {
@@ -51,6 +54,13 @@ public class ContactAddToGroupTests extends TestBase {
     app.group().create(new GroupData().withName("Pokemon"));
     app.goTo().goToHome();
     return  contacts.iterator().next();
+  }
+
+  public GroupData getGroupData(ContactData contact) {
+    Groups groups = app.db().groups();
+    Collection<GroupData> allGroups = new HashSet<>(groups);
+    allGroups.removeAll(contact.getGroups());
+    return allGroups.iterator().next();
   }
 }
 
